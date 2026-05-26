7b53ddaa658f64c0ce71162647c9c5c1e3cd9d36
Mel M. Heravi <mel.heravi@gmail.com>
2026-05-26 13:39:45 -0400

    committed

--- mtk-macleaners.config.js
+++ mtk-macleaners.config.js
@@ -2,7 +2,7 @@ window.MtkMacleanersConfig = {
   app: {
     name: 'MA Cleaners',
     tagline: 'Premium cleaning for homes and small businesses',
-    phone: '(201) 555-0188',
+    phone: '(646) 303-1234',
     email: 'hello@macleaners.local',
     address: 'Paramus, NJ',
     logoText: 'MA',
